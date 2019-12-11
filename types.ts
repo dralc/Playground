e5c8c5053cfdd996608c1a1670e599505ee5f1db
dralc <dr.aloysius@gmail.com>
2019-12-12 09:29:38 +1100

    - add init setup and types

---
+++ types.ts
@@ -0,0 +1,20 @@
+//------- objects --------------------------
+const ob = { a: 1 };
+Object.prototype.toString.call(ob) /*?*/
+// bad
+typeof ob /*?*/
+
+//------- strings --------------------------
+const st = 'a';
+typeof st /*?*/
+new String(st) instanceof String /*?*/
+
+//------- arrays --------------------------
+const ar = ['a'];
+Array.isArray(ar) /*?*/
+Object.prototype.toString.call(ar) /*?*/
+// bad
+typeof ar /*?*/ 
+
+// MORE details : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof#Examples
+
