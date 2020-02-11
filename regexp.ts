e974dbc5cf9ea3266fc28cfec233197df583ace8
dralc <dr.aloysius@gmail.com>
2020-02-11 17:09:04 +1100

    - add important exec() note

--- regexp.ts
+++ regexp.ts
@@ -1,14 +1,16 @@
 function findGroups() {
-	let re = /([\w]*)<mark>([^<>]+)<\/mark>([\w]*)+/ig;
+	let groups_re = /([\w]*)<mark>([^<>]+)<\/mark>([\w]*)+/ig;
 	let text = 'Roc<mark>ket</mark>man some text Moto<mark>rola</mark>s';
 	
 	// String.match() doesn't return groupings :( it just returns all full matches
-	let ar = text.match(re) /*?*/
+	let ar = text.match(groups_re) /*?*/
 	
 	// RegExp.exec() returns a rich result with the groupings :) and all the matches but
 	// you'll have to re-run it until there aren't any more matches to consume
+	// NB. DON'T re-define the regexp within the while loop or you'll keep resetting
+	// exec()'s internal counter to 0
 	let ar2;
-	while (ar2 = re.exec(text)) {
+	while (ar2 = groups_re.exec(text)) {
 		// full match
 		ar2[0] /*?*/
 		//group 1
